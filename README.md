a4f491d3f261c54c97f24efaf7c4f7993be4ce56
Jason <jkirsons@gmail.com>
2021-05-28 22:06:54 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple FOC CAN - CAN Communications - Application Layer
---
+++ src/SimpleFOCCAN.h
@@ -0,0 +1,8 @@
+#ifndef __SIMPLEFOC_CAN_H__
+#define __SIMPLEFOC_CAN_H__
+
+#include "CANDriver.h"
+#include "CANStream.h"
+#include "CANCommander.h"
+
+#endif
